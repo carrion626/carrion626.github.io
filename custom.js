1339be52c03441f08fdbfead16deab314fd3ae46
Nikita <105853444+carrion626@users.noreply.github.com>
2024-01-30 11:37:40 +0300

    Back footer

---
+++ custom.js
@@ -0,0 +1,7 @@
+let tg = window.Telegram.WebApp;
+
+tg.MainButton.text = "Back"; //изменяем текст кнопки 
+tg.MainButton.show()
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    window.location.href = "https://carrion626.github.io/home.html";
+});
