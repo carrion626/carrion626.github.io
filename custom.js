f70866dd8b41226f3ec6e9def806f48ca44f54e1
Nikita <105853444+carrion626@users.noreply.github.com>
2024-02-05 19:53:45 +0300

    смчитсчмти

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-tg.MainButton.text = "Back"; //изменяем текст кнопки 
+tg.MainButton.text = "Назад"; //изменяем текст кнопки 
 tg.MainButton.show()
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
     window.location.href = "https://carrion626.github.io/home.html";
